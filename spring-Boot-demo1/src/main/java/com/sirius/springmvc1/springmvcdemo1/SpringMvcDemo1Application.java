91d1ba87214463c109e7d82689f66fccd2e1587a
sivagasc <sivagasc@gmail.com>
2017-12-21 14:18:22 +0530

    Added Spring Boot app

---
+++ spring-Boot-demo1/src/main/java/com/sirius/springmvc1/springmvcdemo1/SpringMvcDemo1Application.java
@@ -0,0 +1,12 @@
+package com.sirius.springmvc1.springmvcdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcDemo1Application.class, args);
+	}
+}
