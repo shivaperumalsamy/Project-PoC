a13359a126440a2b11c9281460ee0c59db98150c
sivagasc <sivagasc@gmail.com>
2017-12-22 12:02:23 +0530

    no message

--- agconnect/src/main/java/com/sirius/agconnect/HelloController.java
+++ agconnect/src/main/java/com/sirius/agconnect/HelloController.java
@@ -1,4 +1,4 @@
-package com.javatpoint;
+package com.sirius.agconnect;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- agconnect/src/main/webapp/WEB-INF/jsp/welcomepage.jsp
+++ agconnect/src/main/webapp/WEB-INF/jsp/welcomepage.jsp
@@ -1,1 +1,4 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+
 <p>This is welcome page</p>
